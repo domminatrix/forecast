ece761db34e0c6c8ef89e9363228d8c4379f3f83
domminatrix <53867203+domminatrix@users.noreply.github.com>
2019-08-09 09:35:57 +0200

    write content of message in config api file

--- configlistener/run.sh
+++ configlistener/run.sh
@@ -2,6 +2,8 @@
 
 while read msg;
 do
-   echo $msg;
+   rm /etc/forecast/darksky_api_key;
+   touch /etc/forecast/darksky_api_key;
+   echo $msg | tee /etc/forecast/darksky_api_key;
 done < <(mosquitto_sub -h  srvMosquitto -t domminatrix/forecast/configure -q 1)
 #TODO : Etudier le -q
