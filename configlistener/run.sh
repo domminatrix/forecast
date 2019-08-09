1006671237cd8bca7da3986a8b81bbebdc6ee0e3
domminatrix <53867203+domminatrix@users.noreply.github.com>
2019-08-09 10:51:54 +0200

    suppress message file note exist

--- configlistener/run.sh
+++ configlistener/run.sh
@@ -4,7 +4,7 @@
 mosquitto_sub -h  srvMosquitto -t domminatrix/forecast/configure -q 1 | while read RAW_DATA
 do
    mkdir -p /etc/forecast/
-   rm /etc/forecast/darksky_api_key;
+   rm -f /etc/forecast/darksky_api_key;
    touch /etc/forecast/darksky_api_key;
    echo $msg | tee /etc/forecast/darksky_api_key;
 done
