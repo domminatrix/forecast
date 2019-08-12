14625715ec77b378b9d1df88f5fb64f7d44be81d
domminatrix <53867203+domminatrix@users.noreply.github.com>
2019-08-12 17:19:01 +0200

    add lat long config

--- configlistener/run.sh
+++ configlistener/run.sh
@@ -6,5 +6,11 @@ do
    mkdir -p /etc/forecast/
    rm -f /etc/forecast/darksky_api_key;
    touch /etc/forecast/darksky_api_key;
-   echo $RAW_DATA | tee /etc/forecast/darksky_api_key;
+   echo $RAW_DATA | cut -d '_' -f1 | tee /etc/forecast/darksky_api_key;
+   rm -f /etc/forecast/latitude;
+   touch /etc/forecast/latitude;
+   echo $RAW_DATA | cut -d '_' -f2 | tee /etc/forecast/latitude;
+   rm -f /etc/forecast/longitude;
+   touch /etc/forecast/longitude;
+   echo $RAW_DATA | cut -d '_' -f3 | tee /etc/forecast/longitude;
 done
