d5a087e1493b7016e724c937d12f70cc1a23e941
domminatrix <53867203+domminatrix@users.noreply.github.com>
2019-08-13 16:56:56 +0200

    Change api call to lang units and flags

--- apicaller/run.sh
+++ apicaller/run.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 API="https://api.darksky.net/forecast"
+LANG=fr
+UNITS=si
 USER_KEY=$(cat /etc/forecast/darksky_api_key)
 LATITUDE=$(cat /etc/forecast/latitude)
 LONGITUDE=$(cat /etc/forecast/longitude)
-URL="${API}/${USER_KEY}/${LATITUDE},${LONGITUDE}"
+URL="${API}/${USER_KEY}/${LATITUDE},${LONGITUDE}?lang=$(LANG)&units=$(UNITS)&exclude=minutely,hourly,daily,flags"
 echo $URL
 API_RESPONSE=`curl -H "Accept: application/json" ${URL}`
 echo $API_RESPONSE
