65b82d2f36cf5f72c1685ac8be2876f9793c4aad
domminatrix <53867203+domminatrix@users.noreply.github.com>
2019-08-13 17:19:08 +0200

    send values to mqqt topic

--- apicaller/run.sh
+++ apicaller/run.sh
@@ -14,3 +14,29 @@ echo $API_RESPONSE
 
 #TODO : Etudier le -q
 
+PRECIP_INTEN=`echo $API_RESPONSE | jq .currently.precipIntensity`
+mosquitto_pub -h srvMosquitto -t domminatrix/forecast/precip/intensity -q 1 -m "${PRECIP_INTEN}"
+PRECIP_PROB=`echo $API_RESPONSE | jq .currently.precipProbability`
+mosquitto_pub -h srvMosquitto -t domminatrix/forecast/precip/prob -q 1 -m "${PRECIP_PROB}"
+TMP=`echo $API_RESPONSE | jq .currently.temperature`
+mosquitto_pub -h srvMosquitto -t domminatrix/forecast/temperature/real -q 1 -m "${TMP}"
+TMP_APP=`echo $API_RESPONSE | jq .currently.apparentTemperature`
+mosquitto_pub -h srvMosquitto -t domminatrix/forecast/temperature/appear -q 1 -m "${TMP_APP}"
+HUMID=`echo $API_RESPONSE | jq .currently.humidity`
+mosquitto_pub -h srvMosquitto -t domminatrix/forecast/humidity -q 1 -m "${HUMID}"
+PRESS=`echo $API_RESPONSE | jq .currently.pressure`
+mosquitto_pub -h srvMosquitto -t domminatrix/forecast/pressure -q 1 -m "${HUMID}"
+WIND_SPEED=`echo $API_RESPONSE | jq .currently.windSpeed`
+mosquitto_pub -h srvMosquitto -t domminatrix/forecast/wind/speed -q 1 -m "${WIND_SPEED}"
+WIND_RAF=`echo $API_RESPONSE | jq .currently.windGust`
+mosquitto_pub -h srvMosquitto -t domminatrix/forecast/wind/gust -q 1 -m "${WIND_RAF}"
+WIND_DIR=`echo $API_RESPONSE | jq .currently.windBearing`
+mosquitto_pub -h srvMosquitto -t domminatrix/forecast/wind/bearing -q 1 -m "${WIND_DIR}"
+CLOUD=`echo $API_RESPONSE | jq .currently.cloudCover`
+mosquitto_pub -h srvMosquitto -t domminatrix/forecast/cloudcover -q 1 -m "${CLOUD}"
+UV=`echo $API_RESPONSE | jq .currently.uvIndex`
+mosquitto_pub -h srvMosquitto -t domminatrix/forecast/uv -q 1 -m "${UV}"
+VISIB=`echo $API_RESPONSE | jq .currently.visibility`
+mosquitto_pub -h srvMosquitto -t domminatrix/forecast/visibility -q 1 -m "${VISIB}"
+OZONE=`echo $API_RESPONSE | jq .currently.ozone`
+mosquitto_pub -h srvMosquitto -t domminatrix/forecast/ozone -q 1 -m "${OZONE}"
