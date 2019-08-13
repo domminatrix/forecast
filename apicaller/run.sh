6cd61d1ded21be5fe2b9d6abfe67ad89b4888eb6
domminatrix <53867203+domminatrix@users.noreply.github.com>
2019-08-13 16:58:17 +0200

    correct syntax

--- apicaller/run.sh
+++ apicaller/run.sh
@@ -6,7 +6,7 @@ UNITS=si
 USER_KEY=$(cat /etc/forecast/darksky_api_key)
 LATITUDE=$(cat /etc/forecast/latitude)
 LONGITUDE=$(cat /etc/forecast/longitude)
-URL="${API}/${USER_KEY}/${LATITUDE},${LONGITUDE}?lang=$(LANG)&units=$(UNITS)&exclude=minutely,hourly,daily,flags"
+URL="${API}/${USER_KEY}/${LATITUDE},${LONGITUDE}?lang=${LANG}&units=${UNITS}&exclude=minutely,hourly,daily,flags"
 echo $URL
 API_RESPONSE=`curl -H "Accept: application/json" ${URL}`
 echo $API_RESPONSE
