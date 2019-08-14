fbcc8bd83f3e45cec1f20a85ed2738f8407baee5
domminatrix <domminatrix442@gmail.com>
2019-08-14 14:49:00 +0200

    test send mqqt config

--- configlistener/func_send_config.sh
+++ configlistener/func_send_config.sh
@@ -1,75 +1,75 @@
 send_conf_mqtt() {
 local DATA="[ \
   { \
-    \"top\":\"domminatrix\/forecast\/precip\/intensity\", \
+    \"top\":\"domminatrix/forecast/precip/intensity\", \
     \"dir\":0, \
     \"freq\":\"*/10 * * * *\" \
   }, \
   { \
-    \"top\":\"domminatrix\/forecast\/precip\/prob\", \
+    \"top\":\"domminatrix/forecast/precip/prob\", \
     \"dir\":0, \
     \"freq\":\"*/10 * * * *\" \
   }, \
   { \
-    \"top\":\"domminatrix\/forecast\/temperature\/real\", \
-    \"dir\":0, \
-    \"freq\":\"*/10 * * * *\" \
-  }, \
-  
-  { \
-    \"top\":\"domminatrix\/forecast\/temperature\/appear\", \
+    \"top\":\"domminatrix/forecast/temperature/real\", \
     \"dir\":0, \
     \"freq\":\"*/10 * * * *\" \
   }, \
   
   { \
-    \"top\":\"domminatrix\/forecast\/humidity\", \
+    \"top\":\"domminatrix/forecast/temperature/appear\", \
     \"dir\":0, \
     \"freq\":\"*/10 * * * *\" \
   }, \
   
   { \
-    \"top\":\"domminatrix\/forecast\/pressure\", \
+    \"top\":\"domminatrix/forecast/humidity\", \
+    \"dir\":0, \
+    \"freq\":\"*/10 * * * *\" \
+  }, \
+  
+  { \
+    \"top\":\"domminatrix/forecast/pressure\", \
     \"dir\":0, \
     \"freq\":\"*/10 * * * *\" \
   }, \
   { \
-    \"top\":\"domminatrix\/forecast\/wind\/speed\", \
+    \"top\":\"domminatrix/forecast/wind/speed\", \
     \"dir\":0, \
     \"freq\":\"*/10 * * * *\" \
   }, \
   { \
-    \"top\":\"domminatrix\/forecast\/wind\/gust\", \
+    \"top\":\"domminatrix/forecast/wind/gust\", \
     \"dir\":0, \
     \"freq\":\"*/10 * * * *\" \
   }, \
   { \
-    \"top\":\"domminatrix\/forecast\/wind\/bearing\", \
+    \"top\":\"domminatrix/forecast/wind/bearing\", \
     \"dir\":0, \
     \"freq\":\"*/10 * * * *\" \
   }, \
   { \
-    \"top\":\"domminatrix\/forecast\/cloudcover\", \
+    \"top\":\"domminatrix/forecast/cloudcover\", \
     \"dir\":0, \
     \"freq\":\"*/10 * * * *\" \
   }, \
   { \
-    \"top\":\"domminatrix\/forecast\/uv\", \
+    \"top\":\"domminatrix/forecast/uv\", \
     \"dir\":0, \
     \"freq\":\"*/10 * * * *\" \
   }, \
   { \
-    \"top\":\"domminatrix\/forecast\/visibility\", \
+    \"top\":\"domminatrix/forecast/visibility\", \
     \"dir\":0, \
     \"freq\":\"*/10 * * * *\" \
   }, \
   { \
-    \"top\":\"domminatrix\/forecast\/ozone\", \
+    \"top\":\"domminatrix/forecast/ozone\", \
     \"dir\":0, \
     \"freq\":\"*/10 * * * *\" \
   } \
 ]"
-echo $DATA  
+echo "$DATA"  
 mosquitto_pub -h srvMosquitto -t domminatrix/forecast/configure/resp -q 1 -m "${DATA}"
 #TODO : Etudier le -q
 
