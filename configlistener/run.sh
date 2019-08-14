66907e0fd9c95894b652550becea73135d859fbd
domminatrix <domminatrix442@gmail.com>
2019-08-14 15:21:31 +0200

    remove docker volume tmp

--- configlistener/run.sh
+++ configlistener/run.sh
@@ -5,7 +5,6 @@ source func_send_config.sh
 #TODO : Etudier le -q
 mosquitto_sub -h  srvMosquitto -t domminatrix/forecast/configure -q 1 | while read RAW_DATA
 do
-   mkdir -p /etc/forecast/
    rm -f /etc/forecast/darksky_api_key;
    touch /etc/forecast/darksky_api_key;
    echo $RAW_DATA | cut -d '_' -f1 | tee /etc/forecast/darksky_api_key;
